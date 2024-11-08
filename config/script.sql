8ab54f8ae15e33e3a5d98cbae3174375d0abcf3f
AlvinRHD <ezequielhernandes907@gmail.com>
2024-11-08 12:19:24 -0600

    new script

--- config/script.sql
+++ config/script.sql
@@ -69,63 +69,6 @@ LOCK TABLES `clientes` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `detalleorden`
---
-
-DROP TABLE IF EXISTS `detalleorden`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `detalleorden` (
-  `detalle_id` int NOT NULL AUTO_INCREMENT,
-  `orden_id` int NOT NULL,
-  `producto_id` int DEFAULT NULL,
-  `cantidad` int NOT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`detalle_id`),
-  KEY `orden_id` (`orden_id`),
-  KEY `producto_id` (`producto_id`),
-  CONSTRAINT `detalleorden_ibfk_1` FOREIGN KEY (`orden_id`) REFERENCES `ordenes` (`orden_id`),
-  CONSTRAINT `detalleorden_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`producto_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `detalleorden`
---
-
-LOCK TABLES `detalleorden` WRITE;
-/*!40000 ALTER TABLE `detalleorden` DISABLE KEYS */;
-/*!40000 ALTER TABLE `detalleorden` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ordenes`
---
-
-DROP TABLE IF EXISTS `ordenes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ordenes` (
-  `orden_id` int NOT NULL AUTO_INCREMENT,
-  `cliente_id` int NOT NULL,
-  `fechar_orden` datetime DEFAULT CURRENT_TIMESTAMP,
-  `estado` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  PRIMARY KEY (`orden_id`),
-  KEY `cliente_id` (`cliente_id`),
-  CONSTRAINT `ordenes_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`cliente_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ordenes`
---
-
-LOCK TABLES `ordenes` WRITE;
-/*!40000 ALTER TABLE `ordenes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ordenes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `productos`
 --
 
@@ -139,10 +82,11 @@ CREATE TABLE `productos` (
   `precio` decimal(10,2) NOT NULL,
   `cantidad_disponible` int NOT NULL,
   `categoria_id` int DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`producto_id`),
   KEY `categoria_id` (`categoria_id`),
   CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`categoria_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -151,7 +95,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (4,'Sofá','Sofá cómodo para el hogar',399.99,10,3),(5,'Audífonos','Audífonos inalámbricos',49.99,70,1),(6,'Producto nuevo editado','Esta es la descripcion del producto nuevo que acabo de agregar, nueva parte para editar producto nuevo',15.99,30,3);
+INSERT INTO `productos` VALUES (4,'Sofá','Sofá cómodo para el hogar',399.99,10,3,'sofa.jpg'),(5,'Audífonos','Audífonos inalámbricos',49.99,70,1,'audifonosInalambricos.png'),(7,'Tv','TV Samsumg 56.7 pulgadas para visualización de juegos',1577.99,4,1,'tv55.jpg'),(8,'nueva tv','nueva descripcion para tv',12.88,23,1,'tv55.jpg');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -164,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-07 16:32:08
+-- Dump completed on 2024-11-08 12:18:43
